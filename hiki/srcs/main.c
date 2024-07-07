6c672abc4c01913f846f4397f8fbcba53c26d9a0
hi0ki <ansarimehdi518@gmail.com>
2024-07-07 12:20:12 +0100

    hadchi dyal hiki

--- hiki/srcs/main.c
+++ hiki/srcs/main.c
@@ -6,7 +6,19 @@ khasni n9sm cmd o n7to f list achno ba9i lah w3lm
 
 */
 
-int main(int ac, char **av)
+int main()
 {
+	t_list *head;
+	char *line;
 
+		while ((line = readline("minishell $> ")) != NULL)
+		{
+			ft_start(head, line);
+		}
+	printf("howa\n");
+	while(head)
+	{
+		printf("%s\n", head->data);
+		head = head->next;
+	}
 }
--- hiki/srcs/parsing/parsing.c
+++ hiki/srcs/parsing/parsing.c
@@ -1,3 +1,47 @@
 #include "minishell.h"
 
+bool check_special_char(char *str)
+{
+	int i;
 
+	i = 0;
+	// if (str[i] == '>' && str[i + 1] == '>')
+	// 	return (true);=
+	if (str[i] == '<' || str[i] == '>' )
+	{
+		return (true);
+	}
+	return (false);
+}
+
+void ft_start(t_list *head, char *line)
+{
+	int i;
+	int j;
+	char str[255];
+
+	i = 0;
+	j = 0;
+	head = NULL;
+	while(line[i])
+	{
+		if (check_special_char(line + i) == true)
+		{
+			str[j] = '\0';
+			ft_lstadd_back(&head, ft_lstnew(str));
+			printf("%s\n", head->data);
+			ft_bzero(str, j);
+			str[0] = line[i];
+		}
+		if (check_special_char(line + i) == true)
+		{
+			ft_lstadd_back(&head, ft_lstnew(str));
+			printf("%s\n", head->data);
+			ft_bzero(str, j);
+			j = 0;
+		} 
+		str[j] = line[i];
+		j++;
+		i++;
+	}
+}
