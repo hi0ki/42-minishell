35bdb65982517b3459d57bd385f123230a6120f1
hi0ki <ansarimehdi518@gmail.com>
2024-07-13 19:51:10 +0100

    push

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
+#include <unistd.h>
 #include <stdio.h>
 
-int main()
-{
-	printf("\"");
+int main() {
+    // Change the current working directory to "/tmp"
+    if (chdir("joker") == 0) {
+        printf("Changed directory successfully.\n");
+    } else {
+        perror("chdir");
+    }
+
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Current working dir: %s\n", cwd);
+    } else {
+        perror("getcwd");
+    }
+
+    return 0;
 }
